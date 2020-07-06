eb1af104164e5f2ee396f66f0a584df3e8e4c828
Sara-Alcaras <saraalcaras401@gmail.com>
2020-07-06 17:17:07 -0300

    styling register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import './styles.css';
 
 export default function Register() {
-    return <h1>Alooou</h1>
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <h1> Cadastro</h1>
+                    <p>Cadastre seu filme favorito e ajude outras pessoas a sairem do tédio</p>
+                    
+                    <Link className="back-link" to="/register">
+                        Menu
+                    </Link>
+                </section>
+
+                <form>
+                    <input placeholder="Nome do filme" />
+                    <input type="autor" placeholder="Autor" />
+                    <input placeholder="Classificação" />
+                    <input placeholder="Duração" />
+                    <input type="url" placeholder="Link da capa" />
+
+                </form>
+                <button className="fat" type="submit">Cadastrar</button>
+            </div>
+        </div>
+    );
 
 }
