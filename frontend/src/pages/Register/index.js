bbb0561db0703d4c5a9375e63dbc5d4701c994fb
Sara-Alcaras <saraalcaras401@gmail.com>
2020-07-26 19:08:39 -0300

    api connection

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom'
+
+import api from '../../services/api';
 
 import './style.css';
 import '../../global.css';
 
 export default function Register() {
+    const [name, setName] = useState('');
+    const [author, setAuthor] = useState('');
+    const [classification, setClassification] = useState('');
+    const [duration, setDuration] = useState('');
+
+    const history = useHistory();
+
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        const data = {
+            name,
+            author,
+            classification,
+            duration,
+        };
+
+        try {
+            const response = await api.post('movies', data);
+        
+            alert(`Filme cadastrado com sucesso, seu ID: ${response.data.id}`);
+
+            history.push('/');
+
+        } catch(err) {
+            alert('Erro no cadastro, tente novamente.');
+        }
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -26,11 +57,30 @@ export default function Register() {
                 
                 </section>
 
-                <form>
-                    <input placeholder="Nome do filme" />
-                    <input type="autor" placeholder="Autor" />
-                    <input placeholder="Classificação" />
-                    <input placeholder="Duração" />
+                <form onSubmit={handleRegister}>
+                    <input 
+                        placeholder="Nome do filme" 
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <input 
+                        type="autor" 
+                        placeholder="Autor"
+                        value={author}
+                        onChange={e => setAuthor(e.target.value)}
+                    />
+
+                    <input 
+                        placeholder="Classificação"
+                        value={classification}
+                        onChange={e => setClassification(e.target.value)}
+                    />
+
+                    <input 
+                        placeholder="Duração" 
+                        value={duration}
+                        onChange={e => setDuration(e.target.value)}
+                    />
                     
                     <button className="fat" type="submit">Cadastrar</button>
 
